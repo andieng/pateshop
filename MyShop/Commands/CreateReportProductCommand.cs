67a54abbcc4e8bdd77f7ec453f9038e3b0fa6361
congchuabongbong <110735554+cong-chua-bong-bong@users.noreply.github.com>
2024-01-03 20:36:43 +0700

    #report (#6)
    Co-authored-by: thanh <ntnthanh.109@gmail.com>

---
+++ MyShop/Commands/CreateReportProductCommand.cs
@@ -0,0 +1,81 @@
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using MyShop.Services;
+using MyShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MyShop.Commands
+{
+    public class CreateReportProductCommand : BaseCommand
+    {
+        private readonly ReportsViewModel _reportsViewModel;
+
+        public CreateReportProductCommand(ReportsViewModel reportsViewModel)
+        {
+            _reportsViewModel = reportsViewModel;
+        }
+
+        public override async void Execute(object parameter)
+        {
+            int categoryId = _reportsViewModel.Category.CategoryId;
+            var option = _reportsViewModel.OptionProduct.Split(": ")[1];
+            var SoldValues = new List<int>();
+            List<string> xLabels = new List<string>();
+            var data = new List<object>();
+            if (option == "Daily")
+            {
+                var startDate = _reportsViewModel.StartDate;
+                var endDate = _reportsViewModel.EndDate;
+           
+                var year = _reportsViewModel.Year;
+                //call api
+                data = await ShopService.ReportProductDaily(categoryId,startDate, endDate);
+            }
+            else if (option == "Monthly")
+            {
+                var year = _reportsViewModel.Year;
+                //call api
+                data = await ShopService.ReportProductMonthly(categoryId,year);
+            }
+            else if (option == "Yearly")
+            {
+                //call api
+               data = await ShopService.ReportProductYearly(categoryId);
+            }
+            foreach (var item in data)
+            {
+                var jsonElement = (JsonElement)item; // Convert to JsonElement
+
+                var value = Convert.ToInt32(jsonElement.GetProperty("total_quantity_sold").GetString());
+                var name = jsonElement.GetProperty("product_name").GetString();
+
+                SoldValues.Add(value);
+                xLabels.Add(name);
+            }
+            //set x
+            _reportsViewModel.XAxes = new Axis[]
+               {
+            new Axis
+            {
+                Labels = xLabels,
+                TextSize = 12
+            }
+               };
+            //set chart
+
+            _reportsViewModel.Profit_RevenueSeries = new ISeries[]
+            {
+                    new ColumnSeries<int>
+                    {
+                        Values = SoldValues
+                    }
+            };
+        }
+    }
+}
--- MyShop/ViewModels/ReportsViewModel.cs
+++ MyShop/ViewModels/ReportsViewModel.cs
@@ -1,19 +1,168 @@
-﻿using MyShop.Commands;
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using MyShop.Commands;
+using MyShop.Models;
 using System;
 using System.Collections.ObjectModel;
 using System.Configuration;
+using System.Linq;
+using static MyShop.ViewModels.DashboardViewModel;
+using LiveCharts.Wpf;
+using LiveCharts;
+using Axis = LiveChartsCore.SkiaSharpView.Axis;
+using System.Collections.Generic;
+using MyShop.Services;
+using System.Threading.Tasks;
 
 namespace MyShop.ViewModels
 {
     public class ReportsViewModel : BaseViewModel
     {
+        private string _option;
+        private int _year;
+        private int _month;
+        private DateTime _startDate = new DateTime(2023,1,1);
+        private DateTime _endDate = DateTime.Now;
+        private List<Category> _categories;
+        private Category _category;
+        private string _optionProduct;
+
+
+        private Axis[] _xAxes { get; set; }
+        public List<Category> Categories
+        {
+            get => _categories;
+            set
+            {
+                _categories = value;
+                OnPropertyChanged("Categories");
+            }
+        }
+        public Category Category
+        {
+            get => _category;
+            set
+            {
+                _category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+        private ISeries[] _profit_RevenueSeries { get; set; }
+        public ISeries[] Profit_RevenueSeries
+        {
+            get => _profit_RevenueSeries;
+            set
+            {
+                _profit_RevenueSeries = value;
+                OnPropertyChanged(nameof(Profit_RevenueSeries));
+            }
+        }
+        public Axis[] XAxes
+        {
+            get => _xAxes;
+            set
+            {
+                _xAxes = value;
+                OnPropertyChanged(nameof(XAxes));
+            }
+        }
+
+
+        public string Option
+        {
+            get => _option;
+            set
+            {
+                _option = value;
+                OnPropertyChanged("Option");
+            }
+        }
+        public string OptionProduct
+        {
+            get => _optionProduct;
+            set
+            {
+                _optionProduct = value;
+                OnPropertyChanged("OptionProduct");
+            }
+        }
+        public int Year
+        {
+            get => _year;
+            set
+            {
+                _year = value;
+                OnPropertyChanged("Year");
+            }
+        }
+
+        public int Month
+        {
+            get => _month;
+            set
+            {
+                _month = value;
+                OnPropertyChanged("Month");
+            }
+
+        }
+
+        public Array YearValues => new int[] { 2022, 2023 };
+
+        public DateTime EndDate
+        {
+            get => _endDate;
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged("EndDate");
+            }
+        }
+        
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged("StartDate");
+            }
+        }
+   
+
+
+
+       
+
+        public Axis[] YAxes { get; set; } =
+        {
+            new Axis
+            {
+                TextSize = 12,
+            }
+        };
+        public List<string> CategoryNames { get; set; }
+        public BaseCommand CreateReportCommand { get; set; }
+        public BaseCommand CreateReportProductCommand { get; set; }
+
         public ReportsViewModel()
         {
+
+            loadData();
             initCommands();
+        }
+
+        public async Task loadData()
+        {
+            var data = await ShopService.GetAllCategories();
+            Categories =data.Value.Item1;
+            CategoryNames = Categories.Select(item => item.CategoryName).ToList();
         }
 
         public void initCommands()
         {
+            CreateReportCommand = new CreateRevenue_ProfitReportCommand(this);
+            CreateReportProductCommand = new CreateReportProductCommand(this);
         }
     }
 }
